a29bf929639c1f43d9cae995a2cce7f06b711253
satyapavankumarkantamani <kantamani.pavan520@gmail.com>
2020-09-13 09:35:27 +0530

    First commit

---
+++ app/src/main/java/com/sample/exoplayer/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.sample.exoplayer
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    private var videoPlayer: SimpleExoPlayer? = null
+    private var sampleUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initializePlayer()
+    }
+
+
+    private fun buildMediaSource(): MediaSource? {
+        val dataSourceFactory = DefaultDataSourceFactory(this, "sample")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.parse(sampleUrl))
+    }
+
+    private fun initializePlayer() {
+        videoPlayer = SimpleExoPlayer.Builder(this).build()
+        video_player_view?.player = videoPlayer
+        buildMediaSource()?.let {
+            videoPlayer?.prepare(it)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        videoPlayer?.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        videoPlayer?.playWhenReady = false
+        if (isFinishing) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        videoPlayer?.release()
+    }
+}
